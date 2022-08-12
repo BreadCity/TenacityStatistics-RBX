958d9e8aa532135cc5191ea1e0788c7636f2734c
YieldingExploiter <yieldingexploiter@users.noreply.github.com>
2022-08-12 19:24:14 +0200

    feat: :sparkles: add _G support for the one executor without getgenv()

--- public/loader.lua
+++ public/loader.lua
@@ -1,6 +1,8 @@
-if getgenv().TenacityStatistics then return getgenv().TenacityStatistics end
+if getgenv and getgenv().TenacityStatistics or _G.TenacityStatistics then
+  return getgenv and getgenv().TenacityStatistics or _G.TenacityStatistics
+end
 local raw = game:HttpGetAsync('https://tenacity-statistics-rbx.astolfo.gay/public/script.lua');
 local script, err = loadstring(raw);
 if err and (not script or typeof(script) == 'string') then error('Error while loading: ', err) end
 script()
-return getgenv().TenacityStatistics;
+return getgenv and getgenv().TenacityStatistics or _G.TenacityStatistics;
--- src/main.client.ts
+++ src/main.client.ts
@@ -1,6 +1,11 @@
 import api, { APIClass } from 'api';
 
-if (!getgenv()._do_not_globally_expose_tenacity_statistics){
-  getgenv().TenacityStatistics = api;
-  getgenv().__TenacityStatisticsClass = APIClass;
+let genv = getgenv;
+if (!genv)
+// @ts-ignore
+  genv = ()=>_G;
+
+if (!genv()._do_not_globally_expose_tenacity_statistics){
+  genv().TenacityStatistics = api;
+  genv().__TenacityStatisticsClass = APIClass;
 }
