9aefee7bd004bd6b3d96db47cdbc60e0d1ee6b73
YieldingExploiter <yieldingexploiter@users.noreply.github.com>
2022-08-12 18:50:05 +0200

    feat: :sparkles: Add loader

---
+++ public/loader.lua
@@ -0,0 +1,6 @@
+if getgenv().TenacityStatistics then return getgenv().TenacityStatistics end
+local raw = game:HttpGetAsync('https://tenacity-statistics-rbx.astolfo.gay/public/script.lua');
+local script, err = loadstring(raw);
+if err and (not script or typeof(script) == 'string') then error('Error while loading: ', err) end
+script()
+return getgenv().TenacityStatistics;
